91e27d727d87bfaa78ab2c9beb1101a71fe22816
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-02-09 13:55:42 -0600

    Small up date to windows batch file.

--- barcode_maker/barcode.bat
+++ barcode_maker/barcode.bat
@@ -1,5 +1,7 @@
 @echo off
+IF NOT EXIST "C:\PHP5\php.exe" (
 SET /P phpexe=Enter location of php executable: 
+)
 IF ("%phpexe%")==("") set phpexe="C:\PHP5\php.exe"
 %phpexe% -f "%~dp0\barcode.php" %*
 set phpexe=
