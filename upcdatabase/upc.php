03df2ca41c697171047f9e3dc1b7b4d15ce48aa5
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-02-20 16:53:47 -0600

    Added new function get_upca_coupon_info.

--- upcdatabase/upc.php
+++ upcdatabase/upc.php
@@ -277,7 +277,9 @@ if($_GET['act']=="lookup"&&isset($_POST['upc'])&&strlen($_POST['upc'])==13&&
 if($_GET['act']=="add"&&isset($_POST['upc'])&&strlen($_POST['upc'])==13&&
 	validate_ean13($_POST['upc'])===false) { 
 	$_GET['act'] = "lookup"; header("Location: ".$website_url.$url_file."?act=lookup"); }
-if($_GET['act']=="add"&&isset($_POST['upc'])&&preg_match("/^02/", $_POST['upc'])) { 
+if($_GET['act']=="add"&&isset($_POST['upc'])&&
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { 
 	$_GET['act'] = "lookup"; header("Location: ".$website_url.$url_file."?act=lookup&upc=".$_POST['upc']); }
 if($_GET['act']=="add"&&!isset($_COOKIE['MemberName'])&&!isset($_COOKIE['MemberID'])&&
 	!isset($_COOKIE['SessPass'])) { $_GET['act'] = "lookup"; 
@@ -497,16 +499,25 @@ $upcinfo['validated'] = "no"; } } }
   <?php if(!isset($_POST['upc'])) { ?>
   <title> <?php echo $sitename; ?>: Item Lookup </title>
   <?php } if(isset($_POST['upc'])&&$numrows>0&&$upcinfo['validated']=="yes"&&
-	!preg_match("/^02/", $_POST['upc'])) { ?>
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
   <title> <?php echo $sitename; ?>: Item Record </title>
   <?php } if(isset($_POST['upc'])&&$numrows>0&&$upcinfo['validated']=="no"&&
-	!preg_match("/^02/", $_POST['upc'])) { ?>
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
   <title> <?php echo $sitename; ?>: Item Found </title>
   <?php } if(isset($_POST['upc'])&&$numrows===0&&
-	!preg_match("/^02/", $_POST['upc'])) { ?>
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
   <title> <?php echo $sitename; ?>: Item Not Found </title>
-  <?php } if(isset($_POST['upc'])&&preg_match("/^02/", $_POST['upc'])) { ?>
+  <?php } if(isset($_POST['upc'])&&
+	(preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
   <title> <?php echo $sitename; ?>: Random Weight UPC </title>
+  <?php } if(isset($_POST['upc'])&&
+	(!preg_match("/^02/", $_POST['upc'])&&preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
+  <title> <?php echo $sitename; ?>: Dummy UPC </title>
   <?php } ?>
 <?php echo $metatags; ?>
  </head>
@@ -514,7 +525,7 @@ $upcinfo['validated'] = "no"; } } }
  <body>
   <center>
    <?php echo $navbar; ?>
-   <?php if(isset($_POST['upc'])&&preg_match("/^02/", $_POST['upc'])) { 
+   <?php if(isset($_POST['upc'])&&preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])) { 
    $RandWeight = get_upca_vw_info("207362401432");
    $price_split = str_split($RandWeight['price'], 2);
    $RandWeight['price'] = ltrim($price_split[0].".".$price_split[1], "0"); 
@@ -527,10 +538,21 @@ $upcinfo['validated'] = "no"; } } }
    <tr><td width="125">Price</td><td width="50"><?php echo $RandWeight['price']; ?></td></tr>
    </table>
    <div><br /></div>
-   <?php } if(!isset($_POST['upc'])&&!preg_match("/^02/", $_POST['upc'])) { ?>
+   <?php } if(isset($_POST['upc'])&&preg_match("/^04/", $_POST['upc'])&&!preg_match("/^02/", $_POST['upc'])) { 
+   ?>
+   <h2>Dummy UPC</h2>
+   <div>Dummy (number system 4) UPCs are for private use.<br />  This means anybody (typically a retailer) that needs to assign a UPC to an item that doesn't already have one, can use any number system 4 UPC it chooses.<br />  Most importantly, they can know that by doing so, they won't pick one that may already be used.<br />  So, such a UPC can and does mean something different depending on who you ask, and there's no reason to try to keep track of what products these correspond to.</div>
+   <table>
+   <tr><td width="125">UPC-A</td><td width="50"><img src="<?php echo $website_url; ?>barcode.php?act=upca&amp;upc=<?php echo $upca; ?>" alt="<?php echo $upca; ?>" title="<?php echo $upca; ?>" /></td></tr>
+   </table>
+   <div><br /></div>
+   <?php } if(!isset($_POST['upc'])&&
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
    <h2>Item Lookup</h2>
    <?php } if(isset($_POST['upc'])&&$numrows>0&&$upcinfo['validated']=="yes"&&
-	 !preg_match("/^02/", $_POST['upc'])) { ?>
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
    <h2>Item Record</h2>
    <table>
    <?php if($upce!==null&&validate_upce($upce)===true) { ?>
@@ -553,7 +575,8 @@ $upcinfo['validated'] = "no"; } } }
    <!--<br /><br /></div>-->
    <div><br /></div>
    <?php } if(isset($_POST['upc'])&&$numrows>0&&$upcinfo['validated']=="no"&&
-	   !preg_match("/^02/", $_POST['upc'])) { ?>
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
    <h2>Item Found</h2>
    <div>The UPC you were looking for currently is in the database but has not been validated yet.<br /><br /></div>
    <table>
@@ -566,7 +589,9 @@ $upcinfo['validated'] = "no"; } } }
    <?php } ?>
    </table>
    <div><br />Please try coming back later.<br /><br /></div>
-   <?php } if(isset($_POST['upc'])&&$numrows===0&&!preg_match("/^02/", $_POST['upc'])) { ?>
+   <?php } if(isset($_POST['upc'])&&$numrows===0&&
+	(!preg_match("/^02/", $_POST['upc'])&&!preg_match("/^04/", $_POST['upc'])&&
+	!preg_match("/^05/", $_POST['upc'])&&!preg_match("/^09/", $_POST['upc']))) { ?>
    <h2>Item Not Found</h2>
    <div>The UPC you were looking for currently has no record in the database.<br /><br /></div>
    <table>
