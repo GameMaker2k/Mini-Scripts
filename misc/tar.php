5ce619c510914bdf0fcce662ee9b9688b767e516
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2012-08-21 09:00:59 -0500

    Added glob_recursive by Mike @ http://us3.php.net/manual/en/function.glob.php#106595

--- misc/tar.php
+++ misc/tar.php
@@ -12,49 +12,44 @@
     Copyright 2004-2011 Game Maker 2k - http://gamemaker2k.org/
     iTar ver. 0.0.5 by Kazuki Przyborowski
  
-    $FileInfo: tar.php - Last Update: 06/29/2012 Ver 0.0.5 - Author: cooldude2k $
+    $FileInfo: tar.php - Last Update: 08/21/2012 Ver 0.0.5 - Author: cooldude2k $
 */
 
-function file_list_dir($dirname) {
-if (!isset($dirnum)) { $dirnum = null; }
-$srcfile = array();
-$srcdir = array();
-if ($handle = opendir($dirname)) {
-while (false !== ($file = readdir($handle))) {
-      if ($dirnum==null) { $dirnum = 0; }
-	  if ($file != "." && $file != ".." && $file != null) {
-      if(filetype($dirname.$file)=="file") {
-	  $srcfile[$dirnum] = $file; }
-      if(filetype($dirname.$file)=="dir") {
-	  $srcdir[$dirnum] = $file; }
-	  ++$dirnum;
-	  } }
-if($srcdir!=null) { asort($srcdir); }
-if($srcfile!=null) { asort($srcfile); }
-if($srcdir!=null&&$srcfile!=null) {
-$fulllist = array_merge($srcdir, $srcfile); }
-if($srcdir!=null&&$srcfile==null) { $fulllist = $srcdir; }
-if($srcdir==null&&$srcfile!=null) { $fulllist = $srcfile; }
-closedir($handle); }
- return $fulllist; }
+// By: Mike @ http://us3.php.net/manual/en/function.glob.php#106595
+if (!function_exists('glob_recursive'))
+{
+    // Does not support flag GLOB_BRACE
+    function glob_recursive($pattern, $flags = 0)
+    {
+        $files = glob($pattern, $flags);
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+        {
+            $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+        }
+        return $files;
+    }
+}
+
 // PHP iTAR Version 0.0.5
 // license: Revised BSD license
 // Kazuki Przyborowski (http://ja.gamemaker2k.org/)
 function tar($tarfile,$indir1="./",$indir2="") {
 	clearstatcache();
-	$filelist = file_list_dir($indir1);
+	$filelist = glob_recursive($indir1);
 	$tarhandle = fopen($tarfile, "wb+");
 	$i = 0; $num = count($filelist);
 	while ($i < $num) {
-	$fstats=stat($indir1.$filelist[$i]);
+	$rftype=filetype($filelist[$i]);
+	$fstats=stat($filelist[$i]);
 	$fname=str_pad($indir2.$filelist[$i], 100, "\x00");
-	$fmode=str_pad(sprintf('%o', fileperms($indir1.$filelist[$i]))."\x20", 8, "\x00");
+	$fmode=str_pad(sprintf('%o', fileperms($filelist[$i]))."\x20", 8, "\x00");
 	$fowner=str_pad($fstats['uid']."\x20\x00", 8, "\x20", STR_PAD_LEFT);
 	$fgroup=str_pad($fstats['gid']."\x20\x00", 8, "\x20", STR_PAD_LEFT);
 	$fsize=str_pad(decoct($fstats['size'])."\x20", 12, "\x20", STR_PAD_LEFT);
 	$fmtime=str_pad(decoct($fstats['mtime']), 12, "\x20");
 	$fcksum=str_pad("", 8, "\x20", STR_PAD_LEFT);
-	$ftype="0";
+	if($rftype=="dir") { $ftype="5"; }
+	if($rftype=="file") { $ftype="0"; }
 	$flinklist=str_pad("", 100, "\x00");
 	$fulltar=$fname.$fmode.$fowner.$fgroup.$fsize.$fmtime.$fcksum.$ftype.$flinklist;
 	$fulltar=str_pad($fulltar, 512, "\x00");
@@ -68,13 +63,14 @@ function tar($tarfile,$indir1="./",$indir2="") {
 	$fcksum=str_pad(decoct($fckstotal)."\x20\x00", 8, "\x20", STR_PAD_LEFT);
 	$fulltar=$fname.$fmode.$fowner.$fgroup.$fsize.$fmtime.$fcksum.$ftype.$flinklist;
 	$fulltar=str_pad($fulltar, 512, "\x00");
+	if($rftype=="file") {
 	$fbytesize = 0;
-	while ($fbytesize < filesize($indir1.$filelist[$i])) {
+	while ($fbytesize <= filesize($filelist[$i])) {
 	$fbytesize = $fbytesize + 512; }
-	$fulltar=$fulltar.str_pad(file_get_contents($indir1.$filelist[$i]), $fbytesize, "\x00");
+	$fulltar=$fulltar.str_pad(file_get_contents($filelist[$i]), $fbytesize, "\x00");
 	fwrite($tarhandle,$fulltar,strlen($fulltar));
 	$fwstats=fstat($tarhandle);
-	echo ($i + 1)."/".$num." writing: ".$indir2.$filelist[$i]." (".filesize($indir1.$filelist[$i]).") to tar file: ".$tarfile." (".$fwstats['size'].")\n<hr />";
+	echo ($i + 1)."/".$num." writing: ".$indir2.$filelist[$i]." (".filesize($filelist[$i]).") to tar file: ".$tarfile." (".$fwstats['size'].")\n<hr />"; }
 	++$i; }
 	fwrite($tarhandle,str_pad("", 1024, "\x00"), 1024);
 	fclose($tarhandle);
@@ -82,6 +78,6 @@ function tar($tarfile,$indir1="./",$indir2="") {
 function itar($tarfile,$indir1="./",$indir2="") {
 	return tar($tarfile,$indir1,$indir2); }
 echo "<pre>\n<hr />";
-itar("./tfbb.tar", "./tfbb/", "");
+itar("./iDB.tar", "iDB/*", "");
 echo "</pre>";
 ?>
